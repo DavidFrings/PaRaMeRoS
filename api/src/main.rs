3deb4f8ad44e46375bb2413999a96af7c15e0e6c
David Frings, aka DevOpsDavid <dev@davidfrings.dev>
2025-09-18 16:37:09 +0200

    fix: smal functionality fixes

--- api/src/main.rs
+++ api/src/main.rs
@@ -75,7 +75,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         let logger = Logger::default();
         let cors = Cors::default()
-            .allowed_origin("http://127.0.0.1:80")
+            .allowed_origin("http://127.0.0.1:3000")
             .allowed_origin("https://PaRaMeRoS.net")
             .allowed_origin("https://PaRaMeRoS.DavidFrings.dev")
             .allow_any_origin() // Dev
@@ -96,7 +96,7 @@ async fn main() -> std::io::Result<()> {
             .service(new_post)
             .service(update_post)
             .service(delete_post)
-        //.service(health)
+          //.service(health)
     })
     .bind(format!("{}:{}", host, port))?
     .run()
