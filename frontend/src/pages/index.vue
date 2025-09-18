a8d46701ce6ba1ea79cebaf305fb31868463e740
David Frings, aka DevOpsDavid <dev@davidfrings.dev>
2025-09-18 16:41:35 +0200

    fix: small functionality fixes (#64)

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -1,5 +1,4 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
 import ArrowDown from '@/components/icons/ArrowDownIcon.vue'
 import Section from '@/components/SectionComponent.vue'
